6072cb6db63c86539d742774f9f0353d76218ffc
ayaz <ayazzali@mail.ru>
2018-10-02 23:57:56 +0300

    easy BAD auth

---
+++ AuthCore/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Login(string login="guest",string returnUrl = "home")
+        {
+
+            await Authenticate(login);
+            return Redirect(returnUrl);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType,userName)
+            };
+            ///
+            await HttpContext.SignInAsync(
+                new ClaimsPrincipal(
+                    new ClaimsIdentity(
+                        claims,
+                        "ApplicationCookie",
+                        ClaimsIdentity.DefaultNameClaimType,
+                        ClaimsIdentity.DefaultRoleClaimType
+                )));
+        }
+        }
+    }
--- AuthCore/Controllers/HomeController.cs
+++ AuthCore/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AuthCore.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AuthCore.Controllers
 {
@@ -15,6 +16,7 @@ namespace AuthCore.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
